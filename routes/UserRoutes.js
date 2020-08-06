5e2a8f3ab3c40913a37ec9c18197bc6d47637201
jchapan <jchapan@gmail.com>
2020-08-06 18:46:05 -0500

    mas cambios

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -7,8 +7,8 @@ const { verifyToken } = require('../middlewares')
 
 router.get('/users', verifyToken, UserController.findAll)
 router.get('/users/:id', verifyToken, UserController.findOne)
-router.post('/users', verifyToken, UserValidator.create, UserController.create)
-router.put('/users/:id', verifyToken, UserValidator.edit, UserController.edit)
+router.post('/users', UserValidator.create, UserController.create)
+router.put('/users/:id', UserValidator.edit, UserController.edit)
 router.delete('/users/:id', verifyToken, UserController.delete)
 
 module.exports = router
